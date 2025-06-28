f175084fe64b1cfcc496388684706fbb3d855427
RomainV <romain.vartabedian@gmail.com>
2025-06-28 09:44:57 +0200

    ajout config ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,14 @@ module.exports = {
     {
       name: "app",
       script: "./www/app.js",
+      max_memory_restart: "200M", // Limite de mémoire
+      error_file: "./logs/err.log", // Fichier Log des erreurs
+      out_file: "./logs/out.log", 
       env_production: {
         NODE_ENV: "production",
       },
+      instances: 3, // les 3 instances en parallèle
+      exec_mode: "cluster", // Mode cluster pour les instances
     },
   ],
 };
